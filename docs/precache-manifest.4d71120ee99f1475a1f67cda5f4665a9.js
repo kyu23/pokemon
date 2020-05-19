93b4d625510d1131244d19adec718226d9e59acf
Kevin Yu <kevinyu@Kevins-MacBook-Pro.local>
2020-05-19 19:16:59 -0400

    initial

---
+++ docs/precache-manifest.4d71120ee99f1475a1f67cda5f4665a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "803fb334ebf8d4f9d59121e09689e71d",
+    "url": "/pokemon/index.html"
+  },
+  {
+    "revision": "22bee513b7565339454a",
+    "url": "/pokemon/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9f5051b5fe5f1c87e5b2",
+    "url": "/pokemon/static/js/2.ba69a99a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokemon/static/js/2.ba69a99a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22bee513b7565339454a",
+    "url": "/pokemon/static/js/main.d0558936.chunk.js"
+  },
+  {
+    "revision": "cdccaf97c266dc053410",
+    "url": "/pokemon/static/js/runtime-main.4b0b2e80.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+
 
 function App() {
+
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect( () => {
+    showPokemon();
+  }, [pokemon]);
+  
+
+  const showPokemon = (e) => {
+    
+    axios.get(`https://pokeapi.co/api/v2/pokemon/`) /* https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151 */
+      .then( res => setPokemon(res.data.results) )
+      .catch( err => console.log(err) );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <tbody>
+        <tr>
+        <button
+        onClick = { showPokemon }
+        >Fetch Pokemon</button>
+          
+            { 
+              pokemon.map((poke, i) => 
+                  <li key={ i }>{poke.name}</li>
+                )
+            }
+          
+        </tr>
+        
+      </tbody>
+      
     </div>
+    
   );
 }
 
